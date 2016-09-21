d6143824f9e2d67a892565350951c1f61c3161cd
Ali Watters <ali.watters@gmail.com>
2016-09-20 21:01:29 -0700

    updated quickstart app to angular2 final release

--- README.md
+++
@@ -1,5 +0,0 @@
-# Angular2 Quick Start
-
-https://angular.io/docs/ts/latest/quickstart.html
-
-Neat.
---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My Angular 2 Quickstart App</h1>'
+})
+export class AppComponent { }
